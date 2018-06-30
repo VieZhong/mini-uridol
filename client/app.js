96a3d032e2cd9f9815420426a8df6278353dac32
VieZhong <zyw13579@qq.com>
2018-06-30 14:34:06 +0800

    v1

--- client/app.js
+++ client/app.js
@@ -3,7 +3,29 @@ var qcloud = require('./vendor/wafer2-client-sdk/index')
 var config = require('./config')
 
 App({
-    onLaunch: function () {
-        qcloud.setLoginUrl(config.service.loginUrl)
+    onLaunch: function() {
+        // qcloud.setLoginUrl(config.service.loginUrl)
+        wx.cloud.init({
+            // env: 'sft-test-c5bf8f',
+            traceUser: true
+        })
+        wx.login({
+            success: res => {
+                if (res.code) {
+                    wx.cloud.callFunction({
+                        name: 'loginByCode',
+                        data: {
+                            code: res.code,
+                        }
+                    }).then(r => {
+                        console.log(r);
+                    }).catch(err => {
+                        console.log('登陆错误' + err)
+                    })
+                }
+            },
+            fail: err => {}
+        });
+
     }
 })
