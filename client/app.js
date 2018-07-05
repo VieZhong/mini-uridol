144867127f10184550cbd6a38d8e6407e2532e65
VieZhong <zyw13579@qq.com>
2018-07-05 19:17:40 +0800

    production

--- client/app.js
+++ client/app.js
@@ -6,39 +6,50 @@ App({
     onLaunch: function() {
 
         wx.cloud.init({
-          env: 'development-bb7096',
-          traceUser: true
+            env: 'development-bb7096',
+            traceUser: true
         })
 
         //需要判断用户是否登录过
-        const store_key = wx.getStorageSync(STORE_KEY);
-        if (!store_key) {
-            wx.login({
-                success: res => {
-                    if (res.code) {
-                        wx.cloud.callFunction({
-                            name: 'loginByCode',
-                            data: {
-                                code: res.code,
-                            }
-                        }).then(data => {
-                            wx.setStorageSync(STORE_KEY, data);
-                            console.log(data);
-                        }).catch(err => {
-                            console.log('登陆错误' + err)
-                        })
+        // const store_key = wx.getStorageSync(STORE_KEY);
+        // if (!store_key) {
+        wx.login({
+            success: res => {
+                if (res.code) {
+
+                    // wx.cloud.callFunction({
+                    //     name: 'faceCompare',
+                    //     data: {
+                    //         url: "https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=264e6f7072ec54e755e1124cd851f035/7a899e510fb30f2445d60e4dc195d143ad4b032b.jpg",
+                    //     }
+                    // }).then(({ errMsg, result }) => {
+                    //     if (errMsg == "cloud.callFunction:ok") {
+
+                    //         console.log(result);
+                    //     }
+                    // }).catch(err => {
+                    //     console.log('错误' + err)
+                    // })
+
+                    // wx.chooseImage({
+                    //     sizeType: 'original',
+                    //     sourceType: 'camera',
+                    //     success: function(filePaths) {
+                    //         console.log(filePaths)
+                    //     }
+                    // });
 
 
-                    } else {
-                        console.log('登陆错误' + res.errMsg);
-                        util.showModel('登录错误', err.message);
-                    }
-                },
-                fail: err => {
-                    console.log(err);
+                } else {
+                    console.log('登陆错误' + res.errMsg);
                     util.showModel('登录错误', err.message);
                 }
-            })
-        }
+            },
+            fail: err => {
+                console.log(err);
+                util.showModel('登录错误', err.message);
+            }
+        })
+        // }
     },
 })
