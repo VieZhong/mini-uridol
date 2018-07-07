dc69a4f1393994cb7440ad60a00168d763dc37be
tudou <877397589@qq.com>
2018-07-07 21:10:08 +0800

    首页

--- client/pages/similarity/similarity.js
+++ client/pages/similarity/similarity.js
@@ -15,11 +15,9 @@ Page({
             value: value,
             fusePic: fusePic
         });
-    },
-    onReady: function(e) {
         this.innerAudioContext = wx.createInnerAudioContext()
         this.innerAudioContext.autoplay = false;
-        this.innerAudioContext.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
+        this.innerAudioContext.src = music_url;
         this.innerAudioContext.onError((res) => {
             console.log(res.errMsg)
             console.log(res.errCode)
